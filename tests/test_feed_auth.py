50c266cf5abeb322455cf9172217b82e559094ad
Iurii Nartov <yurahttp@gmail.com>
2025-10-02 14:27:57 +0300

    Updating test_feed_auth.py

--- tests/test_feed_auth.py
+++ tests/test_feed_auth.py
@@ -1,16 +1,19 @@
 import pytest
 from src.pages.private.home_feed_page import HomeFeedPage
 
-@pytest.mark.auth
-def test_feed_shows_posts(page):
-    feed = HomeFeedPage(page).open()
-    feed.expect_feed_visible()
-    feed.first_post.expect_visible()
 
-@pytest.mark.auth
-def test_can_like_and_save_first_post(page):
-    feed = HomeFeedPage(page).open()
-    post = feed.first_post
-    post.expect_visible()
-    post.like()
-    post.save()
+class TestFeedAuth:
+
+    @pytest.mark.auth
+    def test_feed_shows_posts(page):
+        feed = HomeFeedPage(page).open()
+        feed.expect_feed_visible()
+        feed.first_post.expect_visible()
+
+    @pytest.mark.auth
+    def test_can_like_and_save_first_post(page):
+        feed = HomeFeedPage(page).open()
+        post = feed.first_post
+        post.expect_visible()
+        post.like()
+        post.save()
