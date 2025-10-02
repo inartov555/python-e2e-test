5804b1961510245c60f4bb598608fc422175349f
Iurii Nartov <yurahttp@gmail.com>
2025-10-02 16:27:14 +0300

    Correcting tests in TestPublicPages

--- tests/test_public_pages.py
+++ tests/test_public_pages.py
@@ -6,20 +6,21 @@ from tools.logger.logger import Logger
 class TestPublicPages:
 
     def test_landing_links_present(page):
+        self.landing_page.open()
         self.landing_page.expect_loaded()
 
     def test_navigate_to_login(page):
-        landing = LandingPage(page).open()
+        self.landing_page.open()
         self.landing_page.go_to_login()
         self.login_page.expect_loaded()
 
     def test_navigate_to_signup(page):
-        landing = LandingPage(page).open()
+        self.landing_page.open()
         self.landing_page.go_to_signup()
         self.signup_page.expect_loaded()
 
     def test_login_negative_incorrect_creds(page):
-        login = LoginPage(page).open()
+        self.login_page.open()
         self.login_page.expect_loaded()
         self.login_page.login("incorrect_user@example.com", "wrong_password")
         # In real runs, expect an error or captcha; we assert the page attempted submission
