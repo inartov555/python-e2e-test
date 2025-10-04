6be2cabc21c785236f796d74999bd676450455a6
Iurii Nartov <yurahttp@gmail.com>
2025-10-04 22:51:15 +0300

    Fixing AttributeError: 'TestPublicPages' object has no attribute 'landing_page'

--- src/pages/private/home_feed_page.py
+++ src/pages/private/home_feed_page.py
@@ -5,10 +5,8 @@ from playwright.sync_api import expect
 from ..base_page import BasePage
 from ...components.post_card import PostCard
 from tools.logger.logger import Logger
-from src.page.conftest import setup_elements_for_test
 
 
-@pytest.mark.usefixtures("setup_elements_for_test")
 class HomeFeedPage(BasePage):
     def __init__(self, url: str, page: Page, request):
         super().__init__(url, page, request)
